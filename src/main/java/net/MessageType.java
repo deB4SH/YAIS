595e450c849b6a4ba511d08eb90d18a92814e656
deB4SH <github@b4sh.de>
2015-11-16 12:38:40 +0100

    merge: dev master

---
+++ src/main/java/net/MessageType.java
@@ -0,0 +1,27 @@
+package net;
+
+public enum MessageType {
+
+    // SIGN IN
+    WELCOME("01-0001"),
+
+    //USER AUTH
+    REQUSERDATA("02-0001"),
+    REQUSER("02-0002"),
+    REQPW("02-0003"),
+
+    //DB REQUEST
+    REQABCASDLKJASDLAS("03-0001");
+
+
+    private String value;
+
+    MessageType(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+
+}
