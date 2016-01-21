8fcd331469d235b142ad1ddd0e9ebdec07114cbf
deB4SH <github@b4sh.de>
2016-01-21 14:07:51 +0100

    FIX-BA: images dones

---
+++ src/test/java/UnitAppTest.java
@@ -0,0 +1,14 @@
+import de.b4sh.yais.mdl.Room;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class UnitAppTest {
+
+    @Test
+    public void testAppTestHelloWorld(){
+        Assert.assertEquals(AppTest.getHelloWorld(), "Hello World");
+    }
+
+
+
+}
