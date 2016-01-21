3592b000691ad570d24b2547b195239e644c700e
deB4SH <github@b4sh.de>
2016-01-21 11:58:08 +0100

    UPDATE: Unittest for model room

--- src/test/java/UnitTest_Model_Room.java
+++ src/test/java/UnitTest_Model_Room.java
@@ -6,28 +6,12 @@ public class UnitTest_Model_Room {
 
     @Test
     public void testRoom(){
-        int id = 0;
-        String val = "Room 301, Haus 21";
-        Room testRoom = new Room(id,val);
-
-        Assert.assertEquals(internalTestRoom(testRoom,id,val),true);
-
+        ModelRoomTest mrt = new ModelRoomTest();
+        mrt.setVal(new Room(0,"Raum 301, Haus 21"),0, "Raum 301, Haus 21");
+        Assert.assertEquals(mrt.internalTestRoom(),true);
     }
 
 
-    private boolean internalTestRoom(Room tr,int id,String val){
-        //check if values are equals
-        if(tr.getLocation().equals(val)){
-            if(tr.getId() == id){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        else {
-            return false;
-        }
-    }
+
 
 }
